3ffa5644f9f0433ba9eb665ca662c37fa3c6b60d
Katharina B <mail@katharina.dev>
2019-06-23 15:39:02 +0200

    change host to heroku

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -20,7 +20,7 @@ class Register extends React.Component {
         this.setState({password: event.target.value})
     }
     onSubmitSignIn = () => {        
-        fetch('http://localhost:3000/register', {
+        fetch('https://cryptic-thicket-28261.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
